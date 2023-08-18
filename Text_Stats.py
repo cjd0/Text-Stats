4c26c7f919a9bc5b8e77c1f5c66130a4530371b9
Chris <chrisdoyle20@gmail.com>
2023-08-18 22:07:41 +0100

    Average word length

--- Text_Stats.py
+++ Text_Stats.py
@@ -10,6 +10,9 @@ class Text_Stats(object):
 
         char_count = str(len(text))
 
+        #used for average word length calculation
+        non_space_count = 0
+
         #do not let this be a permanent solution!
         vowels = ['a','e','i','o','u','A','E','I','O','U']
         consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
@@ -35,8 +38,12 @@ class Text_Stats(object):
             if i in consonants:
                 consonant_count += 1
 
+            if i != " ":
+                non_space_count += 1
 
-        return "Character count: " + char_count + "\nWord count: " + str(word_count) + "\nVowel count: " + str(vowel_count) + "\nConsonant count: " + str(consonant_count)
+
+        return ("Character count: " + char_count + "\nWord count: " + str(word_count) + "\nVowel count: " + str(vowel_count) + "\nConsonant count: " + str(consonant_count) + 
+                "\nAverage word length: " + str(float(non_space_count/word_count)))
 
 
 text_stats_instance = Text_Stats()
